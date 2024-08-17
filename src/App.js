cc451612d6cb3516c3b0c1e078148d239c9a9499
timothytenzin20 <timothytenzin2004@gmail.com>
2024-08-16 23:28:21 -0400

    Configure app to support MUI, Tailwind CSS, and React Router-DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing'
+import Museums from './pages/Museums'
+import Temples from './pages/Temples'
+import Lakes from './pages/Lakes'
+import Historical from './pages/Historical'
+import NoPage from './pages/NoPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index element= {<Landing />}/>
+          <Route path ='/home' element={<Landing/>}/>
+          <Route path ='/museums' element={<Museums/>}/>
+          <Route path ='/temples' element={<Temples/>}/>
+          <Route path ='/lakes' element={<Lakes/>}/>
+          <Route path ='/historical-sites' element={<Historical/>}/>
+          <Route path = "*" element={<NoPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
